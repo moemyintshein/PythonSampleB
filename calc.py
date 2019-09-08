fefa7bff6e3fea9b4c17f9235b3210936774112c
moemyintshein <moemyintshein@outlook.com>
2019-09-08 11:57:31 +0630

    Third commit

---
+++ calc.py
@@ -0,0 +1,5 @@
+width = 20
+height = 5 * 9
+vol = width * height
+
+print(vol)
---
+++ tax.py
@@ -0,0 +1,7 @@
+#Tax Calculator
+
+tax = 50 / 1000
+price = 10000
+totaltax = price * tax
+tprice = price + totaltax
+print(round(tprice))
